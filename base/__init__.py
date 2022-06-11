3fe17433842618dbc46c742e1a1583cda140cbb4
qzart45hi <qwert45hi@yandex.ru>
2022-06-12 01:49:13 +0300

    fix: add InterfaceMode to __init__ of base

--- base/__init__.py
+++ base/__init__.py
@@ -1,3 +1,3 @@
 from ._mub_restx import MUBNamespace
-from .moderators_db import Moderator, Permission, ModPerm
+from .moderators_db import Moderator, Permission, ModPerm, InterfaceMode
 from .permissions import permission_index
