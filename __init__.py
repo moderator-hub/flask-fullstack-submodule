32f80235ed3686d63629e67680e1b46b7168abdc
qzart45hi <qwert45hi@yandex.ru>
2022-07-26 22:25:50 +0300

    fix: __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-from .base import permission_index, Moderator, ModPerm, Permission, mub_base_namespace, MUBNamespace
+from .base import permission_index, Moderator, ModPerm, Permission, mub_base_namespace, MUBController
 from .superuser import superuser_namespace, mub_cli_blueprint
